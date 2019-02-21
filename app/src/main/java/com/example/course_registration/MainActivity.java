76f97d155b17bf99f1c0998f7d5e4702cc6f0ec1
Ali Arshad <al351082@dal.ca>
2019-02-21 13:40:52 -0400

    Created Login Template

--- app/src/main/java/com/example/course_registration/MainActivity.java
+++ app/src/main/java/com/example/course_registration/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.course_registration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+private FirebaseFirestore database;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
     }
 }
