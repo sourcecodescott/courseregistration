9cbff827f68c8338fe55cbb9772f4e29428e552b
Pascha Love-Protter <paschaprotter@TBF39.WPA.Dal.Ca>
2019-02-21 13:19:51 -0400

    Pascha
    Master Upfdate

--- app/src/main/java/com/example/course_registration/MainActivity.java
+++ app/src/main/java/com/example/course_registration/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.course_registration;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = FirebaseFirestore.getInstance();
         ///test
     }
 }
