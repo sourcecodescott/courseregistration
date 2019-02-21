6f2495280f70334cf215d10a1599e0d13e73193a
Ali Arshad <al351082@dal.ca>
2019-02-21 16:06:37 -0400

    Ali and Nick
    Pair Programming
    Login functionality completed. Need to work on connection with the next activity.
    
    Ali: UI development & bug fixes
    Nick: Functionality development & bug fixes

--- app/src/main/java/com/example/course_registration/MainActivity.java
+++ app/src/main/java/com/example/course_registration/MainActivity.java
@@ -3,13 +3,92 @@ package com.example.course_registration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-private FirebaseFirestore database;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference coursebookRef = db.collection("Student");
+    //private DocumentReference courseRef = db.document("Courses/My First Course");
+
+
+    private TextView textViewData;
+    private EditText txtusername;
+    private  EditText txtpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewData = findViewById(R.id.showError);
+        txtusername= findViewById(R.id.editUser);
+        txtpassword = findViewById(R.id.editPassword);
+        //loadCourses();
     }
+
+
+    public void login(View v) {
+        coursebookRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+                        boolean successorfail = false;
+
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Student course = documentSnapshot.toObject(Student.class);
+                            //course.setDocumentId(documentSnapshot.getId());
+
+                            //String documentId = course.getDocumentId();
+
+                            String username = course.getUsername();
+                            String password = course.getPassword();
+
+                            //data +=  "\nCourse Code: " + documentId + "\nTitle: " + title + "\nDescription: " + description
+                            // + "\n\n";
+
+                            if(txtusername.getText().toString().equals(username)&&txtpassword.getText().toString().equals(password))
+                            {
+                                textViewData.setText("Success!");
+                                successorfail = true;
+
+                                //Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+
+                        if(successorfail == false)
+                        {
+                            textViewData.setText("Incorrect username or password.");
+                        }
+                        //textViewData.setText(data);
+                    }
+                });
+    }
+
+
+
+
+
+
 }
