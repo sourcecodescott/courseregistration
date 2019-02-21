0c2d5cc1fd2dae93d6fc45bcbbcc340212100c4b
Pascha Love-Protter <paschaprotter@TBF39.WPA.Dal.Ca>
2019-02-21 14:46:20 -0400

    Pascha and Dan
    Updated course list

--- app/src/main/java/com/example/course_registration/CourseDisplay.java
+++ app/src/main/java/com/example/course_registration/CourseDisplay.java
@@ -4,28 +4,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
 public class CourseDisplay extends AppCompatActivity {
-    private FirebaseFirestore db;
 
-    private RecyclerView recyclerView;
-
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference coursesref = db.collection("Courses");
+    private TextView textViewData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_display);
-        db = FirebaseFirestore.getInstance();
-        private CollectionReference
-        detail.setOn
-        Query query = db.collection("Courses");
-
-
+        textViewData = findViewById(R.id.CourseList);
+        listcourses();
     }
-}
+
+    public void listcourses(){
+        coursesref.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Course course = documentSnapshot.toObject(Course.class);
+                            course.setDocumentId(documentSnapshot.getId());
+
+                            String documentId = course.getDocumentId();
+
+                            String title = course.getCourse_name();
+                            String description = course.getCourse_description();
+
+                            data +=  "\nCourse Code: " + documentId + "\nTitle: " + title + "\nDescription: " + description
+                                    + "\n\n";
+                        }
+                        textViewData.setText(data);
+                    }
+                });
+    }
+    }
+
