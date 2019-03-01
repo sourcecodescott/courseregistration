1578b60c0b21dd2c40a7a37ec914657b1b6d6c65
Samath Scott <sm288145@dal.ca>
2019-03-01 14:39:22 -0400

    Samath and Dan

--- app/src/main/java/com/example/course_registration/MainActivity.java
+++ app/src/main/java/com/example/course_registration/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.course_registration;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,135 +13,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
-import com.example.course_registration.model.Course;
-
-
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-
-    //private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    // private CollectionReference coursebookRef = db.collection("Courses");
-    //private DocumentReference courseRef = db.document("Courses/My First Course");
-
-
-    private RecyclerView recyclerView;
-    private Button addContactButton;
-
-    private FirebaseFirestore database;
-    private FirestoreRecyclerAdapter adapter;
-
-
-    private String name;
-
+    private TextView txtStudentName;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference coursebookRef = db.collection("Student");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtStudentName = findViewById(R.id.textView2);
 
 
-        recyclerView = findViewById(R.id.contactlist);
-        database = FirebaseFirestore.getInstance();
-
-        adapter = setUpAdapter(database);
-        setUpRecyclerView(recyclerView,adapter);
-
+        getFirstandLastName();
 
 
 
 
 
     }
-
-
-    //Connects our recycler view with the viewholder (how we want to show our model[data])
-    // and the firestore adapter
-    private void setUpRecyclerView(RecyclerView rv, FirestoreRecyclerAdapter adapter)
-    {
-        RecyclerView.LayoutManager manager = new LinearLayoutManager(getApplicationContext());
-        rv.setLayoutManager(manager);
-        rv.setItemAnimator(new DefaultItemAnimator());
-        rv.setAdapter(adapter);
-    }
-
-
-    //Creates a Firestore adapter to be used with a Recycler view.
-    //We will see adapter in more details after the midterm
-    //More info on this: https://github.com/firebase/FirebaseUI-Android/blob/master/firestore/README.md
-    private FirestoreRecyclerAdapter setUpAdapter(FirebaseFirestore db)
-    {
-        final Query query = db.collection("Courses").orderBy("course_code").limit(50);
-        FirestoreRecyclerOptions<Course> options = new FirestoreRecyclerOptions.Builder<Course>()
-                .setQuery(query,Course.class)
-                .build();
-
-
-        FirestoreRecyclerAdapter adapter = new FirestoreRecyclerAdapter<Course,ContactViewHolder>(options)
-        {
-            //For each item in the database connect it to the view
-            @Override
-            public void onBindViewHolder(ContactViewHolder holder, int position, final Course model)
-            {
-
-
-
-
-                holder.name.setText(model.getCourse_code());
-                holder.email.setText(model.getCourse_name());
-
-                //Set the on click for the button
-                //I find this ugly :) but it is how you will see in most examples
-                // You CAN use lambadas for the listeners
-                // e.g. setOnClickListener ((View v) -> ....
-                holder.detailsButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this,ViewCourseDetail.class);
-
-                        intent.putExtra("contact",model);
-                        startActivity(intent);
-
-
-
-                    }
-                });
-            }
-
-            @Override
-            public ContactViewHolder onCreateViewHolder(ViewGroup group, int i)
-            {
-                View view = LayoutInflater.from(group.getContext())
-                        .inflate(R.layout.contact_entry,group,false);
-                return new ContactViewHolder(view);
-
-            }
-        };
-
-        return adapter;
-
-    }
-
 
     //Method called every time the activity starts.
     @Override
     protected void onStart() {
         super.onStart();
-
-
         islogggedin();
-
-
-        adapter.startListening();
     }
 
     @Override
@@ -168,11 +81,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        //Tells the adapter to stop listening since we are not using this activity
-        //  anymore. Otherwise the adapter would still exist in the background draining battery
-        //  with useful cycles...
-        adapter.stopListening();
     }
+
+
+
+    public void schedule(View v) {
+
+
+
+    }
+
+    public void courselist(View v) {
+
+        Intent intent = new Intent(MainActivity.this,view_course_list.class);
+
+        startActivity(intent);
+
+    }
+
+
+    public void getFirstandLastName() {
+        coursebookRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+
+                        Globals sharedData = Globals.getInstance();
+                        final String global_username = sharedData.getUsername();
+
+
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Student stu = documentSnapshot.toObject(Student.class);
+
+                            String username = stu.getUsername();
+
+
+                            if(username.equals(global_username))
+                            {
+                                txtStudentName.setText(stu.getName());
+                            }
+
+
+                        }
+
+                    }
+                });
+    }
+
+
 
 
 }
