9f34b9d362e63281af1f263e6a78cb76571e6b64
truisms <46762380+truisms@users.noreply.github.com>
2019-03-06 11:07:52 -0400

    Added RecyclerView to student_schedule_list's xml and began implementing the adapter. Also added new xml layout file course_schedule_entry which contains the layout for the cards used by the RecyclerView in student_schedule_list.

--- app/src/main/java/com/example/course_registration/StudentScheduleList.java
+++ app/src/main/java/com/example/course_registration/StudentScheduleList.java
@@ -2,12 +2,33 @@ package com.example.course_registration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class StudentScheduleList extends AppCompatActivity {
+
+    private RecyclerView courseRecyclerView;
+    private FirebaseFirestore database;
+    private FirestoreRecyclerAdapter databaseToRecycleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_schedule_list);
+
+        courseRecyclerView = findViewById(R.id.courseScheduleRecycler);
+        database = FirebaseFirestore.getInstance();
+
+        databaseToRecycleView = setUpAdapter(database);
+        setUpRecyclerView(courseRecyclerView, databaseToRecycleView);
+    }
+
+    private void setUpRecyclerView(RecyclerView view, FirestoreRecyclerAdapter databaseToRecycleView) {
+        RecyclerView.LayoutManager gridManager = new StaggeredGridLayoutManager(getApplicationContext());
+
+
     }
 }
