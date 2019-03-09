de0cb6003ecf1312d7c8bf2926765b65e006343d
Mat <kallada@cs.dal.ca>
2019-03-09 13:48:32 -0400

    Mathew Kallada and Nick Brisson: Refactoring Unit Tests and Fixes to CI

--- app/src/test/java/com/example/course_registration/ExampleUnitTest.java
+++ app/src/test/java/com/example/course_registration/ExampleUnitTest.java
@@ -7,11 +7,6 @@ import static org.junit.Assert.*;
 
 import java.util.HashMap;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
 
     @Test
