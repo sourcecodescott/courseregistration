336eaba2ecfbb001dcab29b670aa3f0416800421
Ali Arshad <al351082@dal.ca>
2019-03-09 17:37:26 -0400

    Carter and Ali
    JUnit Test for Student Schedule List

---
+++ app/src/androidTest/java/com/example/course_registration/StudentScheduleListTest.java
@@ -0,0 +1,28 @@
+/**
+ * Author: Carter and Ali
+ * JUnit Test for Student Schedule List
+ */
+package com.example.course_registration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentScheduleListTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onStart() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+
+    @Test
+    public void onStop() {
+    }
+}
