61879fd601f611d86f430a1b61d62b92ec0ed643
Pascha Love-Protter <paschaprotter@TFF54.WPA.Dal.Ca>
2019-03-15 17:01:08 -0300

    Greg and Pascha: Update to javadocs and change of variable names

--- app/src/main/java/com/example/course_registration/view_course_list.java
+++ app/src/main/java/com/example/course_registration/view_course_list.java
@@ -1,11 +1,5 @@
 package com.example.course_registration;
 
-
-/**
- * @author Dan and Pascha
- * Coruse list information to display course information and a button to display course details
- */
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-
 import com.example.course_registration.model.Course;
 
 
@@ -26,6 +19,10 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
+/**
+ * @author Dan and Pascha
+ * Coruse list information to display course information and a button to display course details
+ */
 public class view_course_list extends AppCompatActivity {
 
     private RecyclerView recyclerView;
@@ -48,6 +45,11 @@ public class view_course_list extends AppCompatActivity {
 
     }
 
+    /**
+     * Paramaters for Recyclerview
+     * @param rv
+     * @param adapter
+     */
     private void setUpRecyclerView(RecyclerView rv, FirestoreRecyclerAdapter adapter) {
         RecyclerView.LayoutManager manager = new LinearLayoutManager(getApplicationContext());
         rv.setLayoutManager(manager);
@@ -55,6 +57,11 @@ public class view_course_list extends AppCompatActivity {
         rv.setAdapter(adapter);
     }
 
+    /**
+     * Settng up the reference ot the firstore database to created the viewholder.
+     * @param db
+     * @return
+     */
     private FirestoreRecyclerAdapter setUpAdapter(FirebaseFirestore db) {
         final Query query = db.collection("Courses").orderBy("course_code").limit(50);
         FirestoreRecyclerOptions<Course> options = new FirestoreRecyclerOptions.Builder<Course>()
@@ -63,7 +70,12 @@ public class view_course_list extends AppCompatActivity {
 
 
         FirestoreRecyclerAdapter adapter = new FirestoreRecyclerAdapter<Course,CourseViewHolder>(options) {
-
+            /**
+             * Creating the courseviewholder to dispaly all courses 
+             * @param holder
+             * @param position
+             * @param model
+             */
             @Override
             public void onBindViewHolder(CourseViewHolder holder, int position, final Course model) {
                 holder.name.setText(model.getCourse_code());
