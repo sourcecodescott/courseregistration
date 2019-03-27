90f360690a3a49fde1a0809ea32d5a555492b52a
Nick <NickBrisson2@gmail.com>
2019-03-27 16:10:24 -0300

    Nick @ Carter - Espresso test for changing pass
    Initial Espresso test added for changing the password reset button in the new password reset activity.

--- app/src/main/java/com/example/course_registration/resetUserPassword.java
+++ app/src/main/java/com/example/course_registration/resetUserPassword.java
@@ -3,11 +3,9 @@ package com.example.course_registration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-
 public class resetUserPassword extends AppCompatActivity {
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     Globals myglobe = Globals.getInstance();
 
     @Override
