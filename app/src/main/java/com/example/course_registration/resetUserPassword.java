0577ed3683119bb9432136a7ccd37d92c113e1d4
greg <gregbm2@gmail.com>
2019-04-03 16:57:18 -0300

    Greg merge conflict solution

--- app/src/main/java/com/example/course_registration/resetUserPassword.java
+++ app/src/main/java/com/example/course_registration/resetUserPassword.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class resetUserPassword extends AppCompatActivity {
 
 
